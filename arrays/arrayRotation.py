1d0e9f889babaa15bf9f1816255c7d6ef680496b
Jaw <modou.jaw@intel.com>
2019-08-28 23:47:36 -0700

    The first approach in array rotation. Note: If we decide to changed the amount of rotation to 3 we will have to go and change temp to 3.

--- arrays/arrayRotation.py
+++ arrays/arrayRotation.py
@@ -1,48 +1,30 @@
-"""         Objecive ?
-    1. Store the values to rotate in a temp
-    2. store new array copy
-    3.combine new array with the temp array
-    4. Time complexity is 0(n) space complexity is 0(d)
-"""
+
+#TODO : Store [ele 1 & ele 2 ]  in a temp variable
+
+def array_rotation(arr, d, n):
+    """
+    Store ele1 and ele2 in a temp variable
+    loop through the array starting index 3
+    append ele1 and ele2 at the end of the array
+    :param arr:
+    :param d: 2
+    :param n: 7
+    :return: new_ array
+    """
+
+    temp = [arr[0], arr[1]]
+    new_arr = list()
+
+    if len(temp) == d:
+        new_arr = [i for i in arr[2:]]
+        print(new_arr + temp)
+    else:
+        print("The value of temp: {}, should match the value of d: {}".format(len(temp), d))
 
 
-def left_rotate_by_d(arr, n):
-    temp = arr[0], arr[1]
-    result = []
-    for i in arr[2:]:
-        result.append(i)
-    answer = [*result, *temp]
 
 
-
-
-"""
- 1. Store the first element in a temporal variable 
- 2. shift all elements to the left
- 3. store the fist element in the last index
-"""
-
-
-def rotate_by_one(arr, n):
-    temp = arr[0]
-    for i in range(n - 1):
-        arr[i] = arr[i + 1]
-    arr[n - 1] = temp
-
-
-def rotate_by_d(arr, d, n):
-    for i in range(d):
-        rotate_by_one(arr, n)
-
-
-# display the array in horizontal line
-
-def print_array(arr, size):
-    for i in range(size):
-        print("%d"% arr[i], end= " ")
-
-
-if __name__ == "__main__":
+if __name__=="__main__":
     arr = [1, 2, 3, 4, 5, 6, 7]
-    rotate_by_d(arr,2, 7)
-    print(arr)
+    array_rotation(arr, 3, 7)
+
