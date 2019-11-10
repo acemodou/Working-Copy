f2a9a4f20d64fd689bc66770d4fe62249a10af31
Jaw <modou.jaw@intel.com>
2019-11-10 00:18:01 -0800

    Added parallel processing and Classes implementation

---
+++ Classes/dev_1.py
@@ -0,0 +1,18 @@
+from Working-Copy
+
+
+
+
+class Developer():
+    pass
+
+
+
+
+# emp_1 = Developer('Aji', 'Jaw', 50000)
+# emp_2 = Developer('Aisha', 'Jaw', 750000)
+# emp_3 = 'Salem-Jaw-690000'
+
+
+#print(sys.path)
+print("THis is x", x)
