4f5e5dc1e7e2fd01e3ebdc5ae91dea0461a30155
Jaw <modou.jaw@intel.com>
2019-12-01 22:27:16 -0800

    Consecutive ones in binary

--- hacker/prob_10.py
+++ hacker/prob_10.py
@@ -2,13 +2,43 @@ def binary_rep(n):
     bin_res = list()
     while (n >= 1):
         result = n % 2
-        print(result)
         bin_res.append(result)
         n = n // 2
-        print("n is", n)
     bin_res.reverse()
     return bin_res
 
-print(binary_rep(100))
 
-https://www.geeksforgeeks.org/binary-decimal-vice-versa-python/
+
+def decimalToBinary(n):
+    if n > 1:
+        decimalToBinary(n // 2)
+    print(n % 2, end=' ')
+
+def binaryToDecimal(binary):
+    i, decimal = 0, 0
+    while binary != 0:
+        dec = binary % 10
+        decimal += dec * pow(2, i)
+        i +=1
+        binary //=10
+    print(decimal)
+
+def consecutive_ones(decimal):
+    list1 = binary_rep(decimal)
+    print(list1)
+    counter, max_counter = 0, 0
+    for i in range(len(list1)):
+        if list1[i] == 1:
+            counter += 1
+            if counter > max_counter:
+                max_counter = counter
+        else:
+            counter = 0
+    print(max_counter)
+
+
+consecutive_ones(13)
+
+
+
+
