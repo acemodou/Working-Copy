c19639f835d639fe351564f574a501f10162191f
Jaw <modou.jaw@intel.com>
2019-12-23 21:53:31 -0800

    return output file as a new list with integers instead of strings

--- hacker/prob_11.py
+++ hacker/prob_11.py
@@ -1,46 +1,44 @@
 import sys
 
+def get_hour_glass_text_Int(file_path):
+    """
+    open a file, read as list and split to remove new line
+    loop through the list, and assign it to a new list whilst splitting
+    loop through the second list and cast to ints
+    store the new list in the outler list
+    :param file_path:
+    :return: outler list
+    """
 
-def _getvalues(path):
+    with open(file_path, 'r') as f:
+        file_contents = f.read().split('\n')
+        output_list = list()
 
-    with open(path, 'r') as f:
-        lines = f.read()
-        hour_glass = lines.split("\n")
-        output_list = []
-        for rec in hour_glass:
-            numbers_as_strings = rec.split()
-            print(numbers_as_strings)  ## while testing
+        for values in file_contents:
+            store_new_values = values.split()
             inner_list = []
-            for num in numbers_as_strings:
+            for num in store_new_values:
                 inner_list.append(int(num))
             output_list.append(inner_list)
-    #
-    print("\noutput_list =", output_list)
-
+    return output_list
 
 def _hourglass(arr):
-    print("arr is : ", arr)
-    max_value, curr_max = -sys.maxsize, 0
-    print("Max Value = ", max_value)
-    for i in range(len(arr) - 2):
-        for j in range(len(arr) - 2):
-            curr_max = arr[i][j] + arr[i][j+1] + arr[i][j+2] + arr[i+1][j+1] + \
-             arr[i+2][j] + arr[i+2][j+1] + arr[i+2][j+2]
-            print("curr_max", curr_max)
-            if curr_max > max_value:
-                max_value = curr_max
-    print("max_value", max_value)
-    return max_value
+    
+
+
 
 
 if __name__ =="__main__":
+    text_file = get_hour_glass_text_Int('hourglass.txt')
+    print(text_file)
 
-    hour_glass = [[1, 1, 1, 0, 0, 0],
-                  [0, 1, 0, 0, 0, 0],
-                  [1, 1, 1, 0, 0, 0],
-                  [0, 0, 2, 4, 4, 0],
-                  [0, 0, 0, 2, 0, 0],
-                  [0, 0, 1, 2, 4, 0]]
+
+    # hour_glass = [[1, 1, 1, 0, 0, 0],
+    #               [0, 1, 0, 0, 0, 0],
+    #               [1, 1, 1, 0, 0, 0],
+    #               [0, 0, 2, 4, 4, 0],
+    #               [0, 0, 0, 2, 0, 0],
+    #               [0, 0, 1, 2, 4, 0]]
 
     # _hourglass(hour_glass)
-    _getvalues('hourglass.txt')
+
