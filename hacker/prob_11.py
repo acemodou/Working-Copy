e8c01690c43bb4e4ca74a07cd12728a1f801aa1c
Jaw <modou.jaw@intel.com>
2019-12-24 22:45:21 -0800

    hour glass solved

--- hacker/prob_11.py
+++ hacker/prob_11.py
@@ -23,22 +23,21 @@ def get_hour_glass_text_Int(file_path):
     return output_list
 
 def _hourglass(arr):
-    
+    max_value = -sys.maxsize
 
+    print(len(arr))
 
-
+    for row in range(len(arr)-2):
+        for col in range(len(arr) -2):
+            current_val = arr[row][col] + arr[row][col+1] + arr[row][col+2] + \
+                          arr[row+1][col+1] + \
+                          arr[row+2][col] + arr[row+2][col+1] + arr[row+2][col+2]
+            if current_val > max_value:
+                max_value = current_val
+    print("The maximum hour glass is : ({}) ".format(max_value))
+    return max_value
 
 if __name__ =="__main__":
     text_file = get_hour_glass_text_Int('hourglass.txt')
-    print(text_file)
-
-
-    # hour_glass = [[1, 1, 1, 0, 0, 0],
-    #               [0, 1, 0, 0, 0, 0],
-    #               [1, 1, 1, 0, 0, 0],
-    #               [0, 0, 2, 4, 4, 0],
-    #               [0, 0, 0, 2, 0, 0],
-    #               [0, 0, 1, 2, 4, 0]]
-
-    # _hourglass(hour_glass)
+    _hourglass(text_file)
 
