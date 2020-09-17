b6de63d654c12534ed5c4ee24ec1f4e11e2ffbfc
mjaw <modou.jaw@intel.com>
2020-09-16 21:11:53 -0700

    duplicate version 1

--- DataStructures/v1/Python/Arrays/duplicates/Histogram.py
+++ DataStructures/v1/Python/Arrays/duplicates/Histogram.py
@@ -8,7 +8,6 @@ def makeHistogram(seq) -> dict:
         hist[i] = hist.get(i, 0) + 1
     return hist
 
-    
     # hist = [i for i in seq]
     # return hist
 
@@ -30,14 +29,18 @@ def duplicate_count(data):
     return dupCount
 
 def checkIfDuplicates(data):
-    ''' check if give list have duplicates '''
+    ''' check if given list have duplicates '''
     if len(data) == len(set(data)):
         return 'There are no dups'
     else:
         return 'There are dups'
 
 def checkDuplicates(data):
-    '''Check if given list contains duplicates'''
+    '''
+    Check if given list contains duplicates
+    This ends once we found the first duplicate
+
+    '''
     setOfElems = set()
     for elems in data:
         if elems in setOfElems:
@@ -46,11 +49,55 @@ def checkDuplicates(data):
             setOfElems.add(elems)
     return 'There are no dups'
 
+def dupCountSort(data):
+    #TODO: Sort the data 
+    #TODO: start from the first index to the last 
+    #TODO: check if the current index = prev index or next index 
+    #TODO: Count first and last separately to avoid array bound errors 
+
+    dupCount = 0
+    data.sort()
+    for i in range(1, len(data) - 1):
+        if data[i] == data[i-1] or data[i] == data[i+1]:
+            dupCount +=1
+
+    'Count first and last separately to avoid array bound errors'
+    if data[0] == data[1]:
+        dupCount +=1
+    if data[len(data) -1] == data[len(data) -2]:
+        dupCount +=1
+
+    return dupCount
+
+def dupCountHalfCompare(data):
+    #TODO: compare the two data and if a match is found 
+    #TODO: Increment match 
+    #TODO: Determine number duplicate based on matches
+    #TODO: If there is a match increment dupCount by 2
+    #TODO: If match is 1 increment dupCount by 1
+
+    dupCount = 0
+    for i in range(1, len(data)):
+        match = 0
+        for compare in range(i -1):
+            if data[i] == data[compare]:
+                match +=1
+
+        if match == 1: 
+            dupCount +=2
+        if match > 1:
+            dupCount +=1
+            
+    return dupCount
+
+
 
 
 if __name__=="__main__":
     data = [20, 3, 19, 4, 14, 5, 12, 4, 3, 6, 1, 17, 8, 4, 6, 15, 1, 2, 7, 12]
     # print(f'Histogram duplicate count: {makeHistogram(data)} \n')
-    print(f'Histogram duplicate count: {duplicate_count(data)} \n')
+    #print(f'Histogram duplicate count: {duplicate_count(data)} \n')
     #print(f'Histogram duplicate count: {checkIfDuplicates(data)} \n')
     # print(f'Histogram duplicate count: {checkDuplicates(data)} \n')
+    #print(f'Histogram duplicate count: {dupCountSort(data)} \n')
+    print(f'Histogram duplicate count: {dupCountHalfCompare(data)} \n')
