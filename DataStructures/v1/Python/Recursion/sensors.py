0e0ead9dcb26bf76255e85f9c8e051d7813139f6
mjaw <modou.jaw@intel.com>
2020-09-30 22:58:30 -0700

    convert iterative approach to recursive module

--- DataStructures/v1/Python/Recursion/sensors.py
+++ DataStructures/v1/Python/Recursion/sensors.py
@@ -11,10 +11,6 @@ def totalDiff(senosorA, senosorB, size):
 def totalDiffDispatcher(senosorA, senosorB, size):
     '''
     Convert iterative approach to recursive
-    1. Make an iterative approach 
-    2. Create a dispatcher function, setup a base case
-    3. call the iterative module in the dispatcher
-    4. convert iterative module to call the dispatcher.
     '''
     if size == 0: return 0
     lastElementDiff = abs(senosorA[size -1] - senosorB[size -1])
