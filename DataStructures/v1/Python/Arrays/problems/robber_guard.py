fd01ab91478fe2bc7fb7339a00388091229aa25e
mjaw <modou.jaw@intel.com>
2020-10-04 01:03:01 -0700

    Reorganizing folders.

---
+++ DataStructures/v1/Python/Arrays/problems/robber_guard.py
@@ -0,0 +1,6 @@
+#TODO; find degrees using sin Inverse . Height, width is giving in the program
+#TODO: Find the hypotenuse of the angle and return the degrees 
+#TODO: Ratio of x is sine of the angle 
+#TODO: Change X is the ratio * robberspeed 
+#TODO: change in Y is cosine * robberspeed
+#TODO: Understand this first.
